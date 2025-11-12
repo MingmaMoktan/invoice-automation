600de88f4c3c7d8f764dfb2136280b5db3b851a0
sb13512 <sb13512@student.samk.fi>
2025-11-12 21:23:12 +0200

    Added invoice file model

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class InvoiceFile(models.Model):
+    file = models.FileField(upload_to='invoices/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    extracted_text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.file.name
