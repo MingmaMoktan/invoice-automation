67eb1829839d52b5b8dddfb180c6a82d541bdecb
sb13512 <sb13512@student.samk.fi>
2025-12-02 16:26:07 +0200

    Made the signals.py simple to avoid the unnecessary .save cells and inconsistent profile creation

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,16 +1,11 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import UserProfile  # your profile model
+from .models import UserProfile
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        # Create a new profile if the user was just created
-        UserProfile.objects.create(user=instance)
-    else:
-        # Save existing profile (if it exists)
-        try:
-            instance.userprofile.save()
-        except UserProfile.DoesNotExist:
-            UserProfile.objects.create(user=instance)
+        UserProfile.objects.get_or_create(user=instance)
